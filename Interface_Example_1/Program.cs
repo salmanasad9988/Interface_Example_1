7538ffd4909d6b743fefdf8ba41c95e8362f3bcb
Salman Asad <salmanasad9988@gmail.com>
2023-10-11 09:38:25 +0500

    Add project files.

---
+++ Interface_Example_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Define an interface
+public interface IDrawable
+{
+    void Draw();
+}
+
+// Implement the interface in a class
+public class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+public class Square : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a square");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create instances
+        IDrawable circle = new Circle();
+        IDrawable square = new Square();
+
+        // Call the method defined in the interface
+        circle.Draw();
+        square.Draw();
+    }
+}
